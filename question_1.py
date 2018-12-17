2e552d8f30e011a7160371dc7168a5787f5d17de
xytywh <1804595021@qq.com>
2018-12-17 21:51:48 +0800

    修改

--- question_1.py
+++ question_1.py
@@ -1,6 +1,6 @@
 # 如果 a+b+c=1000，且 a^2+b^2=c^2（a,b,c 为自然数），如何求出所有a、b、c可能的组合?
-for i in range(0, 1000):
-    for j in range(0, 1000):
-        for k in range(0, 1000):
+for i in range(0, 1001):
+    for j in range(0, 1001):
+        for k in range(0, 1001):
             if i + j + k == 1000 and i * i + j * j == k * k:
                 print(i, j, k)
