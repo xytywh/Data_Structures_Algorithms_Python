d34f009dde9114be7b3755ace6c88f788dc2f485
xytywh <1804595021@qq.com>
2018-12-21 10:42:22 +0800

    自己根据single_link_list改的代码

---
+++ double_link_list.py
@@ -0,0 +1,131 @@
+class Node():
+    """节点"""
+
+    def __init__(self, elem):
+        self.elem = elem
+        self.pre = None
+        self.next = None
+
+
+class DoubleLinkList():
+    """双向链表"""
+
+    def __init__(self, node=None):
+        # __head 双下划线代表是私有变量
+        self.__head = node
+
+    def is_empty(self):
+        """链表是否为空"""
+        return self.__head == None
+
+    def length(self):
+        """链表长度"""
+        # cur游标，用来移动遍历节点
+        cur = self.__head
+        count = 0
+        while cur != None:
+            count += 1
+            cur = cur.next
+        return count
+
+    def travel(self):
+        """遍历整个链表"""
+        cur = self.__head
+        while cur != None:
+            print(cur.elem, end=" ")
+            cur = cur.next
+        print("")
+
+    def add(self, item):
+        """链表头部添加元素，头插法"""
+        node = Node(item)
+        node.next = self.__head
+        node.pre = None
+        self.__head = node
+
+    def append(self, item):
+        """链表尾部添加元素，尾插法"""
+        node = Node(item)
+        if self.is_empty():
+            self.__head = node
+        else:
+            cur = self.__head
+            while cur.next != None:
+                cur = cur.next
+            cur.next = node
+            node.pre = cur
+
+    def insert(self, pos, item):
+        """指定位置添加元素
+        :param pos 从0开始
+        """
+        if pos <= 0:
+            self.add(item)
+        elif pos > self.length() - 1:
+            self.append(item)
+        else:
+            pre_ = self.__head
+            count = 0
+            while count < (pos - 1):
+                count += 1
+                pre_ = pre_.next
+            # 当循环退出时，pre指向pos-1位置
+            node = Node(item)
+            node.next = pre_.next
+            node.pre = pre_
+            pre_.next = node
+
+    def remove(self, item):
+        """删除节点"""
+        pre_ = None
+        cur = self.__head
+        while cur != None:
+            if cur.elem == item:
+                # 先判断此结点是否是头结点
+                if cur == self.__head:
+                    self.__head = cur.next
+                else:
+                    pre_.next = cur.next
+                    cur.pre = pre_
+                # 删除元素之后，退出
+                break
+            else:
+                pre_ = cur
+                cur = cur.next
+
+    def search(self, item):
+        """查找节点是否存在"""
+        cur = self.__head
+        while cur != None:
+            if cur.elem == item:
+                return True
+            cur = cur.next
+        return False
+
+
+# 测试
+if __name__ == "__main__":
+    ll = DoubleLinkList(Node(80))
+    print(ll.is_empty())
+    print(ll.length())
+    ll.append(1)
+    ll.travel()
+    print(ll.is_empty())
+    print(ll.length())
+
+    ll.add(8)
+    ll.append(2)
+    ll.append(3)
+    ll.append(4)
+    ll.append(5)
+    ll.append(6)
+    ll.insert(-1, 9)
+    ll.travel()
+    ll.insert(10, 7)
+    ll.travel()
+    ll.insert(2, 3)
+    ll.travel()
+    print(ll.search(10))
+    ll.remove(9)
+    ll.travel()
+
