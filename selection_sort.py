a2084180412e94d71769a68c18e7199d814b5373
xytywh <1804595021@qq.com>
2018-12-25 21:31:57 +0800

    新文件

---
+++ selection_sort.py
@@ -0,0 +1,14 @@
+def selection_sort(list_):
+    """选择排序"""
+    for i in range(len(list_) - 1):
+        for j in range(i + 1, len(list_)):
+            if list_[i] > list_[j]:
+                list_[i], list_[j] = list_[j], list_[i]
+        # print(list_)
+    return list_
+
+
+a = [1, 2, 4, 3, 5, 7, 6, 8, 10, 9, 12, 14, 13, 11, 1, 2, 3]
+# 传递a[:]，不会改变a的值
+print(selection_sort(a[:]))
+# print(a)
