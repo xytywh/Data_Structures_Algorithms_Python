8f3e2d589b3c4fac36c2b549b277b6073ac4efbb
xytywh <1804595021@qq.com>
2018-12-25 16:53:18 +0800

    修改

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,5 +1,5 @@
 def bubble_sort(list):
-    for i in range(0, len(list)):
+    for i in range(len(list)):
         for j in range(i + 1, len(list)):
             if list[i] > list[j]:
                 list[i], list[j] = list[j], list[i]
