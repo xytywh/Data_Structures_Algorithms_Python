2790505c9330cde4bc2a5e6c2c47eb07e4bdfaff
xytywh <1804595021@qq.com>
2018-12-27 14:06:38 +0800

    修改

--- selection_sort.py
+++ selection_sort.py
@@ -8,6 +8,16 @@ def selection_sort(list_):
     return list_
 
 
+# def selection_sort(list_):
+#     """选择排序(不稳定排序)"""
+#     for i in range(len(list_) - 1):
+#         for j in range(len(list_) - 1, i, -1):
+#             if list_[j] < list_[j - 1]:
+#                 list_[j - 1], list_[j] = list_[j], list_[j - 1]
+#         print(list_)
+#     return list_
+
+
 a = [1, 2, 4, 3, 5, 7, 6, 8, 10, 9, 12, 14, 13, 11, 1, 2, 3]
 # 传递a[:]，不会改变a的值
 print(selection_sort(a[:]))
