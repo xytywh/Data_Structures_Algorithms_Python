cfa1f37d9e8b0462fb2b2dd0bd54a2f31438236c
xytywh <1804595021@qq.com>
2019-01-13 13:46:25 +0800

    新文件

---
+++ problem-solving-with-algorithms-and-data-structure-using-python书中代码/chapter-4/code4.1_iteration_sum.py
@@ -0,0 +1,8 @@
+def list_sum(num_list):
+    the_sum = 0
+    for i in num_list:
+        the_sum = the_sum + i
+    return the_sum
+
+
+print(list_sum([1, 3, 5, 7, 9]))
---
+++ problem-solving-with-algorithms-and-data-structure-using-python书中代码/chapter-4/question-15.py
@@ -0,0 +1,9 @@
+def list_sum(num_list):
+    if len(num_list) == 1:
+        return num_list[0]
+    # 下面两句都行，等价的
+    return list_sum(num_list[:-1]) + num_list[-1]
+    # return num_list[0] + list_sum(num_list[1:])
+
+
+print(list_sum([2, 4, 6, 8, 10]))
