e11fb25a908b16660c937b0bbb0446282f0dbd62
xytywh <1804595021@qq.com>
2019-01-23 21:56:04 +0800

    添加自己的实现

--- problem-solving-with-algorithms-and-data-structure-using-python书中代码/chapter-5 排序与搜索/insertion_sort.py
+++ problem-solving-with-algorithms-and-data-structure-using-python书中代码/chapter-5 排序与搜索/insertion_sort.py
@@ -16,21 +16,39 @@
 #     return list_
 
 
+# def insertion_sort(list_):
+#     """插入排序(稳定排序)"""
+#     # 从第二个位置，即下标为1的元素开始向前插入
+#     for i in range(1, len(list_)):
+#         # 从第i个元素开始向前比较，如果小于前一个元素，交换位置
+#         for j in range(i, 0, -1):
+#             if list_[j] < list_[j - 1]:
+#                 list_[j], list_[j - 1] = list_[j - 1], list_[j]
+#             # 加上else，可以使最好时间复杂度为O(n)
+#             else:
+#                 # 左半部分已经有序了，所以如果无序部分的第一个元素不比
+#                 # 有序部分的最后一个元素小的话，代表此元素已经有序了
+#                 # 可以退出了，不需要再比了
+#                 break
+#         # print(list_)
+#     return list_
+
+# 我自己的实现
 def insertion_sort(list_):
-    """插入排序(稳定排序)"""
-    # 从第二个位置，即下标为1的元素开始向前插入
     for i in range(1, len(list_)):
-        # 从第i个元素开始向前比较，如果小于前一个元素，交换位置
-        for j in range(i, 0, -1):
-            if list_[j] < list_[j - 1]:
-                list_[j], list_[j - 1] = list_[j - 1], list_[j]
-            # 加上else，可以使最好时间复杂度为O(n)
+        temp = list_[i]
+        flag = False
+        for j in range(i - 1, -1, -1):
+            # 把前面有序的部分往后移动，空出的位置就是当前元素要插入的位置
+            if temp < list_[j]:
+                flag = True
+                list_[j + 1] = list_[j]
             else:
-                # 左半部分已经有序了，所以如果无序部分的第一个元素不比
-                # 有序部分的最后一个元素小的话，代表此元素已经有序了
-                # 可以退出了，不需要再比了
                 break
-        # print(list_)
+        # 只有当循环里面发生移动的时候，才插到当前位置，否则，代表当前元素位置是对的
+        if flag:
+            list_[j + 1] = temp
+        print(list_)
     return list_
 
 
