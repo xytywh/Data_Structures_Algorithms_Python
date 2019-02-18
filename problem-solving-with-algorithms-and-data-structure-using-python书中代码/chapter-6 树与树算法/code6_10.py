6aff396c40a72b1a389d4dcbf939e57326630f38
xytywh <1804595021@qq.com>
2019-02-18 22:21:09 +0800

    新文件

---
+++ problem-solving-with-algorithms-and-data-structure-using-python书中代码/chapter-6 树与树算法/code6_10.py
@@ -0,0 +1,47 @@
+class BinaryTree:
+    def __init__(self, rootObj):
+        self.key = rootObj
+        self.leftChild = None
+        self.rightChild = None
+
+    def insertLeft(self, newNode):
+        if self.leftChild is None:
+            self.leftChild = BinaryTree(newNode)
+        else:
+            t = BinaryTree(newNode)
+            t.leftChild = self.leftChild
+            self.leftChild = t
+
+    def insertRight(self, newNode):
+        if self.rightChild is None:
+            self.rightChild = BinaryTree(newNode)
+        else:
+            t = BinaryTree(newNode)
+            t.rightChild = self.rightChild
+            self.rightChild = t
+
+    def getRootVal(self):
+        return self.key
+
+    def setRootVal(self, newVal):
+        self.key = newVal
+
+    def getLeftChild(self):
+        return self.leftChild
+
+    def getRightChild(self):
+        return self.rightChild
+
+
+if __name__ == '__main__':
+    r = BinaryTree('a')
+    print(r.getRootVal())
+    print(r.getLeftChild())
+    r.insertLeft('b')
+    print(r.getLeftChild())
+    print(r.getLeftChild().getRootVal())
+    r.insertRight('c')
+    print(r.getRightChild())
+    print(r.getRightChild().getRootVal())
+    r.getRightChild().setRootVal('hello')
+    print(r.getRightChild().getRootVal())
